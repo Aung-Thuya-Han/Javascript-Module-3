c3ac83273d1ad2e52e8b106b0a2588a9c4dc5d43
aungthuyahan <aungthu.ya659@gmail.com>
2025-11-11 17:41:25 +0630

    2nd Commit

---
+++ t2/2.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const target = document.getElementById("target");
+
+const first = document.createElement('li');
+first.textContent = "First item";
+target.appendChild(first);
+
+const second = document.createElement('li');
+second.textContent = "Second item";
+target.appendChild(second)
+
+const third = document.createElement('li');
+third.textContent = "Third item";
+target.appendChild(third)
